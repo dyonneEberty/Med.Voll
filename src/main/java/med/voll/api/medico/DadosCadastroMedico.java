e49d02d7feb28d61da685f58895e4c4a23c41bf3
dyonne <bydyonne@gmail.com>
2024-06-08 13:14:19 -0300

    Some implamentations 08/06

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,13 +1,24 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
 // Java cria uma classe imutavel, cada um dos campos tera os respectivos atributos, cria os getters e setters, cria o construtor. Não precisando digitar essa parte de codigo.
 public record DadosCadastroMedico(
+        @NotBlank
         String nome,
+        @NotBlank
+        @Email
         String email,
+        @NotBlank @Pattern(regexp = "\\d{4,6}" )
         String crm,
+        @NotNull
         Especialidade especialidade,
+        @NotNull @Valid
         DadosEndereco endereco) {
 
 }
