da295f1281dc0ab91725da8caaa3f4c8604398b5
dyonne <bydyonne@gmail.com>
2024-06-11 22:40:44 -0300

    some implementations on TratadorDeErros.class

--- src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -1,4 +1,34 @@
 package med.voll.api.infra;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class TratadorDeErros {
+
+    // Fix Erro 404
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarErro404(){
+
+        return ResponseEntity.notFound().build();
+    }
+
+    // Fix Erro 400
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem){
+
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
 }
