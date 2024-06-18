1e1f6aaeebc256b4f2666b40cf9955a089cfa9dc
dyonne <bydyonne@gmail.com>
2024-06-18 13:37:03 -0300

    Implementation of class responsible for scheduling appointments

--- src/main/java/med/voll/api/domain/consulta/AgendaConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaConsultas.java
@@ -3,9 +3,13 @@ package med.voll.api.domain.consulta;
 import med.voll.api.domain.ValidacaoExcepetion;
 import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.DadosDetalhamentoPaciente;
 import med.voll.api.domain.paciente.PacienteRepository;
+import med.voll.api.domain.validacoes.ValidadorAgendamentoDeConsulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AgendaConsultas {
@@ -19,7 +23,10 @@ public class AgendaConsultas {
     @Autowired
     private PacienteRepository pacienteRepository;
 
-    public void agendar (DadosAgendamentoConsulta dados){
+    @Autowired
+    private List<ValidadorAgendamentoDeConsulta> validadores;
+
+    public DadosDetalhamentoConsulta agendar (DadosAgendamentoConsulta dados){
         if (!pacienteRepository.existsById(dados.idPaciente())) {
             throw new ValidacaoExcepetion("Id do paciente informado não existe!");
         }
@@ -28,10 +35,14 @@ public class AgendaConsultas {
             throw new ValidacaoExcepetion("Id do medico informado não existe!");
         }
 
+        validadores.forEach(v -> v.validar(dados));
+
         var medico = escolherMedico(dados);
         var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
         var consulta = new Consulta(null, medico, paciente, dados.data());
         consultaRepository.save(consulta);
+
+        return new DadosDetalhamentoConsulta(consulta);
     }
 
     private Medico escolherMedico(DadosAgendamentoConsulta dados) {
